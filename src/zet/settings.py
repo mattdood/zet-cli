fd15a6de4d2f954e69ef049c232e12ced9e604e9
mattdood <mawimberly@csumb.edu>
2021-05-07 20:08:07 -0700

    settings and templates added for ease of reference for defaults

---
+++ src/zet/settings.py
@@ -0,0 +1,7 @@
+import os
+
+ZET_PROJECT = os.path.dirname(os.path.abspath(__file__))
+
+# Default directory for sets
+ZET_DEFAULT = "~/zets/"
+ZET_TEMPLATE = os.path.join(ZET_PROJECT, "templates/readme.md")
