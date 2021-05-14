3fd49f451f58b9dfee4630820019ec324f7f8050
mattdood <mawimberly@csumb.edu>
2021-05-13 17:53:24 -0700

    working on main argparsing

--- src/zet/main.py
+++ src/zet/main.py
@@ -6,12 +6,74 @@ def main():
     parser = argparse.ArgumentParser(
         prog="zet", description="Zettlekasten command line tools"
     )
+    subparsers = parser.add_subparsers(help="sub-command help")
 
-    parser.add_argument()
+    parser_create = subparsers.add_parser("create", help="Creates a zet.")
+    parser_create.add_argument(
+        "-t", "--title", action="store", required=True, help="A zet title."
+    )
+    parser_create.add_argument(
+        "-f",
+        "--folder",
+        action="store",
+        help="A zet repo folder name. Defaults to ZET_DEFAULT_FOLDER.",
+    )
+    parser_create.add_argument(
+        "-template", "--template", action="store", help="A zet template name."
+    )
 
-    parser.add_argument("-t", "--title", action="store", help="a zet title")
+    parser_list = subparsers.add_parser("list", help="List zets from a folder.")
+    parser_list.add_argument(
+        "-f",
+        "--folder",
+        action="store",
+        help="A zet repo folder, must be in environment variables. Defaults to ZET_DEFAULT_FOLDER.",
+    )
+    parser_list.add_argument(
+        "-full",
+        "--full_path",
+        action="store",
+        help="Full paths to zets. Defaults to false.",
+    )
 
-    parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
+    parser_git_init = subparsers.add_parser("init", help="Git init inside folder.")
+    parser_git_init.add_argument(
+        "-f",
+        "--folder",
+        action="store",
+        help="A zet repo folder, must be in environment variables. Defaults to ZET_DEFAULT_FOLDER.",
+    )
+
+    parser_git_add = subparsers.add_parser(
+        "add", help="Git add all zets inside folder."
+    )
+    parser_git_add.add_argument(
+        "-f",
+        "--folder",
+        action="store",
+        help="A zet repo folder, must be in environment variables. Defaults to ZET_DEFAULT_FOLDER.",
+    )
+
+    parser_git_commit = subparsers.add_parser(
+        "commit", help="Git commit zets in folder."
+    )
+    parser_git_commit.add_argument(
+        "-m", "--message", action="store", help="Commit message. Defaults to zet name."
+    )
+    parser_git_commit.add_argument(
+        "-f",
+        "--folder",
+        action="store",
+        help="A zet repo folder, must be in environment variables. Defaults to ZET_DEFAULT_FOLDER.",
+    )
+
+    parser_git_push = subparsers.add_parser("push", help="Git push zets in folder.")
+    parser_git_push.add_argument(
+        "-f",
+        "--folder",
+        action="store",
+        help="A zet repo folder, must be in environment variables. Defaults to ZET_DEFAULT_FOLDER.",
+    )
 
 
 if __name__ == "__main__":
