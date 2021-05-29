05eda12c1b0e9bf70de4a421174d0739977141ce
mattdood <mawimberly@csumb.edu>
2021-05-29 13:35:16 -0700

    added env tests and working on the add_repo command

--- src/zet/env_setup.py
+++ src/zet/env_setup.py
@@ -1,11 +1,11 @@
+import json
 import os
 import shutil
-from pathlib import Path
 
 from src.zet.settings import ZET_ENV_PATH, ZET_HOME
 
 
-def create_env(env_path: str = ZET_ENV_PATH, project_path: Path = ZET_HOME) -> None:
+def create_env() -> None:
     """Creates a zet env file.
 
     Params:
@@ -17,11 +17,35 @@ def create_env(env_path: str = ZET_ENV_PATH, project_path: Path = ZET_HOME) -> N
     Returns:
         None
     """
-    example_path = os.path.join(project_path, ".env/.example.json")
+    example_path = os.path.join(ZET_HOME, ".env/.example.json")
 
-    if not os.path.exists(env_path):
-        shutil.copyfile(example_path, env_path)
+    if not os.path.exists(ZET_ENV_PATH):
+        shutil.copyfile(example_path, ZET_ENV_PATH)
 
 
-def add_repo(zet_repo: str, folder: str, env_path: str = ZET_ENV_PATH):
-    pass
+def add_repo(zet_repo: str, zet_path: str) -> None:
+    """Adds a new repo to the env file.
+
+    Params:
+        zet_repo (str): A zet repo name to
+            append to an existing env file.
+
+    Returns:
+        None
+    """
+
+    if not os.path.exists(ZET_ENV_PATH):
+        create_env()
+
+    zet_repo_path = os.path.join(zet_path, zet_repo)
+    if not os.path.exists(zet_repo_path):
+        os.makedirs(zet_repo_path)
+
+    new_repo = {zet_repo: zet_repo_path}
+
+    with open(ZET_ENV_PATH, "r+") as file:
+        zet_repos = json.load(file)["zet_repos"]
+        zet_repos.update(new_repo)
+        json.dump(zet_repos, file, indent=8)
+    file.close()
+
