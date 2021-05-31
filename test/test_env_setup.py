db11a4d176a42ed02694a95716ffacab8689af4d
mattdood <mawimberly@csumb.edu>
2021-05-30 22:30:41 -0700

    tests now work properly on env setup

--- test/test_env_setup.py
+++ test/test_env_setup.py
@@ -2,25 +2,22 @@ import json
 import os
 
 from src.zet.env_setup import add_repo, create_env
-from src.zet.settings import ZET_ENV_PATH
+from src.zet.settings import ZET_DEFAULT_FOLDER, ZET_ENV_PATH
 
 
-def test_env_creates():
-    create_env()
-    assert os.path.exists(ZET_ENV_PATH)
-
-    with open(ZET_ENV_PATH, "r") as file:
-        zet_env = json.load(file)["zet_repos"]
-        assert {} == zet_env
+def test_env_creates(tmp_path):
+    tmp_env_path = os.path.join(str(tmp_path), ".local.json")
+    create_env(tmp_env_path)
+    assert os.path.exists(tmp_env_path)
 
 
-def test_add_repo(tmp_path, zet_env_file):
+def test_add_repo(zet_tmp_env, tmp_path):
     zet_repo = "some_repo"
-    zet_path = os.path.join(str(tmp_path), zet_repo)
-    add_repo(zet_repo, zet_path)
+    zet_path = os.path.join(tmp_path, zet_repo)
+    add_repo(zet_repo, str(tmp_path), zet_tmp_env)
     assert os.path.exists(zet_path)
 
-    with open(ZET_ENV_PATH, "r") as file:
+    with open(zet_tmp_env, "r") as file:
         zet_env = json.load(file)["zet_repos"]
-        print(zet_env)
-        assert {"some_repo": zet_path} == zet_env
+        assert {zet_repo: zet_path} == zet_env
+
