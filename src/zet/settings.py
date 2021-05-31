b76bd304037d544df26840e5f3aed3768ffc3d5f
mattdood <mawimberly@csumb.edu>
2021-05-30 22:30:26 -0700

    reworked settings to properly pull folders

--- src/zet/settings.py
+++ src/zet/settings.py
@@ -2,21 +2,25 @@ import json
 import os
 from pathlib import Path
 
+
 ZET_PROJECT = Path(__file__)
 ZET_HOME = ZET_PROJECT.parents[2]
 
 ZET_ENV_PATH = os.path.join(ZET_HOME, ".env/.local.json")
-
-if not os.path.exists(ZET_ENV_PATH):
-    from .env_setup import create_env
-
-    create_env(ZET_ENV_PATH, ZET_HOME)
 
 # Default directory for sets
 ZET_DEFAULT_FOLDER = {"zets": "~/zets/"}
 ZET_DEFAULT_EDITOR = {"editor": "vim", "command": "vim"}
 ZET_DEFAULT_TEMPLATE = os.path.join(ZET_HOME, "src/zet", "templates/readme.md")
 
-ZET_ENV_JSON = open(ZET_ENV_PATH)
-ZET_FOLDERS = json.load(ZET_ENV_JSON)["zet_repos"]
-ZET_FOLDERS.update(ZET_DEFAULT_FOLDER)
+if not os.path.exists(ZET_ENV_PATH):
+    from .env_setup import add_repo, create_env
+    create_env()
+    default_key = list(ZET_DEFAULT_FOLDER.keys())[0]
+    add_repo(default_key, ZET_DEFAULT_FOLDER[default_key])
+
+config = open(ZET_ENV_PATH, "r")
+ZET_FOLDERS = json.load(config)["zet_repos"]
+config.close()
+
+print(ZET_FOLDERS)
