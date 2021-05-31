307969fbf7eb338a231790bfbeb47545d4cf593d
mattdood <mawimberly@csumb.edu>
2021-05-30 22:29:58 -0700

    reworked env setup to remove circular dependencies and take optional settings path

--- src/zet/env_setup.py
+++ src/zet/env_setup.py
@@ -2,40 +2,32 @@ import json
 import os
 import shutil
 
-from src.zet.settings import ZET_ENV_PATH, ZET_HOME
+from src.zet.settings import ZET_DEFAULT_FOLDER, ZET_ENV_PATH, ZET_HOME
 
 
-def create_env() -> None:
+def create_env(env_path: str = ZET_ENV_PATH) -> None:
     """Creates a zet env file.
-
-    Params:
-        env_path (str): String with the
-            `.local.json` path.
-        project_path (str): String with the
-            ZET_PROJECT path.
-
-    Returns:
-        None
     """
     example_path = os.path.join(ZET_HOME, ".env/.example.json")
 
-    if not os.path.exists(ZET_ENV_PATH):
-        shutil.copyfile(example_path, ZET_ENV_PATH)
+    if not os.path.exists(env_path):
+        shutil.copyfile(example_path, env_path)
 
 
-def add_repo(zet_repo: str, zet_path: str) -> None:
+def add_repo(zet_repo: str, zet_path: str, env_path: str = ZET_ENV_PATH) -> None:
     """Adds a new repo to the env file.
 
     Params:
         zet_repo (str): A zet repo name to
             append to an existing env file.
+        zet_path (str): The path to a new
+            zet repo.
+        env_path (str): A user's `.local.json`
+            environment path.
 
     Returns:
         None
     """
-
-    if not os.path.exists(ZET_ENV_PATH):
-        create_env()
 
     zet_repo_path = os.path.join(zet_path, zet_repo)
     if not os.path.exists(zet_repo_path):
@@ -43,9 +35,13 @@ def add_repo(zet_repo: str, zet_path: str) -> None:
 
     new_repo = {zet_repo: zet_repo_path}
 
-    with open(ZET_ENV_PATH, "r+") as file:
-        zet_repos = json.load(file)["zet_repos"]
-        zet_repos.update(new_repo)
-        json.dump(zet_repos, file, indent=8)
-    file.close()
+    # Loading the entire file then truncating
+    # and re-writing it is pretty inefficient
+    # but at least the file is small?
+    config_file = open(env_path, "r+")
+    config_data = json.load(config_file)
+    repos = config_data["zet_repos"]
+    repos.update(new_repo)
+    config_file.seek(0)
+    json.dump(config_data, config_file, indent = 4)
 
