db5ef49712d4ac1d31fd1c4a5f928592cd851c20
mattdood <mawimberly@csumb.edu>
2021-05-31 22:04:13 -0700

    editor now properly opens, defaults to vim

--- src/zet/editor_commands.py
+++ src/zet/editor_commands.py
@@ -1,8 +1,10 @@
-import subprocess
+import os
+from subprocess import call
 from typing import Dict
 
 from .settings import ZET_DEFAULT_EDITOR
 
 
 def open_editor(path: str, editor: Dict = ZET_DEFAULT_EDITOR):
-    return subprocess.check_output([editor["command"], path])
+    EDITOR = os.environ.get('EDITOR', editor["command"])
+    return call([EDITOR, path])
