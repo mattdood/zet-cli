d2c433125ca2931183ebf8c95606657bab3bbcb6
mattdood <mawimberly@csumb.edu>
2021-07-29 16:18:53 -0700

    test coverage for git commands, git commands moved to new settings

--- src/zet/git_commands.py
+++ src/zet/git_commands.py
@@ -1,26 +1,26 @@
 import subprocess
 from typing import Dict
 
-from .settings import ZET_DEFAULT_FOLDER, ZET_FOLDERS
+from .settings import Settings
+from .settings import ZET_DEFAULT_REPO, ZET_LOCAL_ENV_PATH
 
 
-def git_init_zets(zet_repo: str = ZET_DEFAULT_FOLDER["zets"], folder: Dict[str, str] = ZET_FOLDERS):
+def git_init_zets(zet_repo: str = ZET_DEFAULT_REPO):
     """Initializes a git repo.
 
     Params:
         zet_repo (str): A zet repo name.
             Defaults to ZET_DEFAULT_FOLDER.
-        folder (Dict[str, str]): A dictionary
-            of zet folders. Defaults to ZET_FOLDERS.
 
     Returns:
         subprocess (Pipe): Output is the terminal
             messages of the bash command.
     """
-    return subprocess.check_output(['git', 'init'], cwd = folder[zet_repo])
+    repo = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")[zet_repo]["folder"]
+    return subprocess.check_output(['git', 'init'], cwd = repo)
 
 
-def git_add_zets(zet_repo: str = ZET_DEFAULT_FOLDER["zets"], folder: Dict[str, str] = ZET_FOLDERS):
+def git_add_zets(zet_repo: str = ZET_DEFAULT_REPO):
     """Adds all files to staging in a repo.
 
     Params:
@@ -33,10 +33,11 @@ def git_add_zets(zet_repo: str = ZET_DEFAULT_FOLDER["zets"], folder: Dict[str, s
         subprocess (Pipe): Output is the terminal
             messages of the bash command.
     """
-    return subprocess.check_output(['git', 'add', '.'], cwd = folder[zet_repo])
+    repo = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")[zet_repo]["folder"]
+    return subprocess.check_output(['git', 'add', '.'], cwd = repo)
 
 
-def git_commit_zets(message: str, zet_repo: str = ZET_DEFAULT_FOLDER["zets"], folder: Dict[str, str] = ZET_FOLDERS):
+def git_commit_zets(message: str, zet_repo: str = ZET_DEFAULT_REPO):
     """Performs git commit in a repo.
 
     Params:
@@ -50,10 +51,11 @@ def git_commit_zets(message: str, zet_repo: str = ZET_DEFAULT_FOLDER["zets"], fo
         subprocess (Pipe): Output is the terminal
             messages of the bash command.
     """
-    return subprocess.check_output(['git', 'commit', '-m', message], cwd = folder[zet_repo])
+    repo = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")[zet_repo]["folder"]
+    return subprocess.check_output(['git', 'commit', '-m', message], cwd = repo)
 
 
-def git_push_zets(zet_repo: str = ZET_DEFAULT_FOLDER["zets"], folder: Dict[str, str] = ZET_FOLDERS):
+def git_push_zets(zet_repo: str = ZET_DEFAULT_REPO):
     """Remote pushes a zet repo.
 
     Params:
@@ -66,17 +68,19 @@ def git_push_zets(zet_repo: str = ZET_DEFAULT_FOLDER["zets"], folder: Dict[str, 
         subprocess (Pipe): Output is the terminal
             messages of the bash command.
     """
-    return subprocess.check_output(['git', 'push'], cwd = folder[zet_repo])
+    repo = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")[zet_repo]["folder"]
+    return subprocess.check_output(['git', 'push'], cwd = repo)
 
 
-def git_pull_zets(folders: Dict[str, str] = ZET_FOLDERS) -> None:
+def git_pull_zets() -> None:
     """Pulls all changes for every repo.
 
     Params:
         folder (Dict[str, str]): A dictionary
             of zet folders. Defaults to ZET_FOLDERS.
     """
-    keys = list(folders.keys())
+    repos = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")
+    keys = list(repos.keys())
     for key in keys:
-        subprocess.check_output(['git', 'pull'], cwd = folders[key])
+        subprocess.check_output(['git', 'pull'], cwd = repos[key]["folder"])
 
--- test/test_git_commands.py
+++ test/test_git_commands.py
@@ -8,23 +8,18 @@ from src.zet.git_commands import git_init_zets
 from src.zet.git_commands import git_push_zets
 
 
-def test_git_init_initializes(zet_test_repo, zet_folders):
-    os.makedirs(zet_folders[zet_test_repo] + "test_dir")
-    git_init = git_init_zets(zet_test_repo, zet_folders)
+def test_git_init_initializes(zet_test_repo):
+    git_init = git_init_zets(zet_test_repo)
     assert "Initialized empty Git repository" in str(git_init)
 
 
 def test_git_add_adds_changes(zet_test_repo, zet_git_repo):
-    new_file = open(zet_git_repo[zet_test_repo] + "some_file.md", "w")
-    new_file.close()
-    git_add = git_add_zets(zet_test_repo, zet_git_repo)
+    git_add = git_add_zets(zet_test_repo)
     assert "" in str(git_add)
 
 
 def test_git_commit_zets(zet_test_repo, zet_git_repo_changes):
-    print("before commit")
-    git_commit = git_commit_zets("some message", zet_test_repo, zet_git_repo_changes)
-    print("after commit")
+    git_commit = git_commit_zets("some message", zet_test_repo)
     assert "some message" in str(git_commit)
     assert "files changed" in str(git_commit) or "file changed" in str(git_commit)
 
