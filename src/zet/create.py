e7bc87538f7b8a13e7796801e360c7d674c2ce71
mattdood <mawimberly@csumb.edu>
2021-07-29 16:16:43 -0700

    zet create migrated to new settings, test coverage for create

--- src/zet/create.py
+++ src/zet/create.py
@@ -5,15 +5,15 @@ import shutil
 import time
 from typing import Dict, List
 
-from .settings import ZET_DEFAULT_KEY, ZET_DEFAULT_TEMPLATE, ZET_FOLDERS
+from .settings import Settings
+from .settings import ZET_DEFAULT_REPO, ZET_DEFAULT_TEMPLATE, ZET_LOCAL_ENV_PATH
 
 
 def create_zet(
     title: str,
     category: str,
     tags: str,
-    zet_repo: str = ZET_DEFAULT_KEY,
-    folder: Dict[str, str] = ZET_FOLDERS,
+    zet_repo: str = ZET_DEFAULT_REPO,
     template: str = ZET_DEFAULT_TEMPLATE,
 ) -> str:
     """Creates a new zet.
@@ -26,8 +26,6 @@ def create_zet(
         title (str): Title of the zet,
             does not replace filename.
         zet_repo (str): A zet repo name.
-        folder (str): String with the
-            parent folder path.
         template (str): Template path
             for the file.
 
@@ -40,8 +38,10 @@ def create_zet(
     today_month = str(today.month)
     today_str = str(today.strftime("%Y%m%d%H%M%S"))
 
+    repo = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")[zet_repo]["folder"]
+
     full_path = os.path.join(
-        folder[zet_repo], today_year, today_month, today_str
+        repo, today_year, today_month, today_str
     )
     clean_title = title.lower().replace(' ', '-')
     full_title = str(clean_title) + "-" + today_str + ".md"
@@ -75,8 +75,7 @@ def create_zet(
 
 def bulk_import_zets(
     files_folder: str,
-    zet_repo: str = ZET_DEFAULT_KEY,
-    folder: Dict[str, str] = ZET_FOLDERS,
+    zet_repo: str = ZET_DEFAULT_REPO,
 ) -> List:
     """Bulk create zets from a folder.
 
@@ -87,8 +86,6 @@ def bulk_import_zets(
         files_folder (str): A folder with
             pre-existing files ready to import.
         zet_repo (str): A zet repo name.
-        folder (str): String with the
-            parent folder path.
 
     Returns:
         zet_list (List): A list of dict objects
@@ -97,12 +94,13 @@ def bulk_import_zets(
     """
 
     zet_list = []
+    repo = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")[zet_repo]["folder"]
     for root, dirs, files in os.walk(files_folder):
         for file in files:
             today = datetime.datetime.now()
             today_str = str(today.strftime("%Y%m%d%H%M%S"))
             full_path = os.path.join(
-                folder[zet_repo], str(today.year), str(today.month), today_str
+                repo, str(today.year), str(today.month), today_str
             )
 
             filename = file.split(".")[0]
--- test/test_create.py
+++ test/test_create.py
@@ -6,46 +6,38 @@ import pytest
 from src.zet.create import bulk_import_zets, create_zet
 
 
-def test_zet_exists(zet_test_repo, zet_folders):
+def test_zet_exists(zet_settings):
     zet = create_zet(
         "some title",
         "some category",
-        "some, tags",
-        zet_test_repo,
-        zet_folders
+        "some, tags"
     )
     assert os.path.exists(zet)
 
 
-def test_unique_zets(zet_test_repo, zet_folders):
+def test_unique_zets(zet_settings):
 
     zet_one = create_zet(
         "some title",
         "some category",
         "some, tags",
-        zet_test_repo,
-        zet_folders
     )
     time.sleep(1)
     zet_two = create_zet(
         "some title",
         "some category",
-        "some, tags",
-        zet_test_repo,
-        zet_folders
+        "some, tags"
     )
     assert zet_one != zet_two
     assert os.path.exists(zet_one)
     assert os.path.exists(zet_two)
 
 
-def test_zet_metadata(zet_test_repo, zet_folders):
+def test_zet_metadata(zet_settings):
     zet = create_zet(
         "some title",
         "some category",
-        "some, tags",
-        zet_test_repo,
-        zet_folders
+        "some, tags"
     )
     zet_file = open(zet)
     text = ""
@@ -60,7 +52,7 @@ def test_zet_metadata(zet_test_repo, zet_folders):
     assert "templateTags" not in text
 
 
-def test_bulk_create_zets(tmp_path, zet_test_repo, zet_folders):
+def test_bulk_create_zets(tmp_path, zet_settings):
 
     for i in range(5):
         folder_name = "some_folder_" + str(i)
@@ -70,12 +62,11 @@ def test_bulk_create_zets(tmp_path, zet_test_repo, zet_folders):
         p = d / file_name
         p.write_text("some test text")
 
-    zet_list = bulk_import_zets(tmp_path, zet_test_repo, zet_folders)
+    zet_list = bulk_import_zets(tmp_path)
 
     for zet in zet_list:
         assert os.path.exists(zet["existing_path"])
         assert os.path.exists(zet["zet_file_path"])
-        print(zet["zet_file_path"])
         zet_file = open(zet["zet_file_path"])
         text = ""
         for line in zet_file:
