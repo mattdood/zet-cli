0f54ff6bb18bdef70956945b9c171983dc9be474
mattdood <mawimberly@csumb.edu>
2021-08-03 20:28:36 -0700

    settings adapted to have instantiated obj with refresh(), append and update separated, global constants defined

--- src/zet/settings.py
+++ src/zet/settings.py
@@ -1,7 +1,7 @@
 import json
 import os
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
 
 
 ZET_PROJECT = Path(__file__)
@@ -15,10 +15,18 @@ ZET_DEFAULT_TEMPLATE = os.path.join(ZET_HOME, "src/zet", "templates/readme.md")
 
 
 class Settings:
+    """Object to interact with `.local.json`.
+    """
 
     def __init__(self, path: str):
         self.path = path
         self.data = self.load_settings(self.path)
+
+
+    def refresh(self):
+        self.data = self.load_settings(self.path)
+        return self
+
 
     @staticmethod
     def load_settings(path: str) -> Dict:
@@ -26,27 +34,51 @@ class Settings:
             data = json.load(file)
         return data
 
-    def get_setting(self, key: str):
-        return self.data[key]
 
-    def update_setting(self, key: str, value) -> None:
+    def get_setting(self, key: str = None):
+        # TODO: Test no key
+        if key:
+            return self.data[key]
+        else:
+            return self.data
+
+
+    def set_item(self, settings, keys: List[str], value) -> None:
+        key = keys.pop(0)
+        try:
+            self.set_item(settings[key], keys, value)
+        except (IndexError, KeyError):
+            settings[key] = value
+
+
+    def update_setting(self, keys: List[str], value) -> None:
         settings_file = open(self.path, "r+")
         data = json.load(settings_file)
-        data[key].update(value)
+        self.set_item(data, keys, value)
         settings_file.seek(0)
         json.dump(data, settings_file, indent=4)
-        settings_file.close()
+
+
+    def append_setting(self, key: str, value) -> None:
+        settings_file = open(self.path, "r+")
+        config_data = json.load(settings_file)
+        data = config_data[key]
+        data.update(value)
+        settings_file.seek(0)
+        json.dump(config_data, settings_file, indent=4)
 
 
 if not os.path.exists(ZET_LOCAL_ENV_PATH):
-    from src.zet.env_setup import generate_env
+    from .env_setup import generate_env
     generate_env()
 
-local_settings = Settings(ZET_LOCAL_ENV_PATH)
+
+local_settings = Settings(ZET_LOCAL_ENV_PATH).refresh()
 ZET_DEFAULTS = local_settings.get_setting("defaults")
+ZET_DEFAULT_EDITOR = ZET_DEFAULTS["editor"]
+ZET_DEFAULT_REPO = ZET_DEFAULTS["repo"]
+ZET_DEFAULT_TEMPLATE = ZET_DEFAULTS["template"]
+
 ZET_REPOS = local_settings.get_setting("zet_repos")
 ZET_TEMPLATES = local_settings.get_setting("templates")
-
-ZET_DEFAULT_REPO = "zets"
-ZET_DEFAULT_FOLDER = {ZET_DEFAULT_REPO: "zets/"}
 
