1ced2f80b92e1b04089b9837d6e56c93565c618e
mattdood <mawimberly@csumb.edu>
2021-08-03 20:29:05 -0700

    reworked settings tests to reflect the refresh, append, and update

--- test/test_settings.py
+++ test/test_settings.py
@@ -1,28 +1,49 @@
-import json
-from typing import Dict
-
 import pytest
 
-from src.zet.env_setup import generate_env
-from src.zet.settings import Settings
-from src.zet.settings import ZET_LOCAL_ENV_PATH
+from src.zet.settings import Settings, ZET_LOCAL_ENV_PATH
 
 
-def test_settings_retrieval():
-    generate_env()
+def test_settings_retrieval(zet_settings):
     default_settings = Settings(ZET_LOCAL_ENV_PATH).get_setting("defaults")
     assert isinstance(default_settings, dict)
 
+    settings = Settings(ZET_LOCAL_ENV_PATH)
+    data = settings.get_setting()
+    assert data != None
+    assert isinstance(data, dict)
 
-def test_settings_update():
-    generate_env()
-    new_repo = {
+def test_settings_append_dict(zet_settings):
+    key = "zet_repos"
+    value = {
         "some_name": {
             "folder": "some/path/",
             "template": "default"
         }
     }
-    Settings(ZET_LOCAL_ENV_PATH).update_setting("zet_repos", new_repo)
+    Settings(ZET_LOCAL_ENV_PATH).append_setting(key, value)
     new_settings = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")
-    assert any(new_repo["some_name"] == new_settings[repo] for repo in new_settings)
+    assert any(value["some_name"] == new_settings[repo] for repo in new_settings)
+
+def test_settings_update_key(zet_settings):
+    keys = ["defaults", "editor", "name"]
+    value = "vscode"
+    Settings(ZET_LOCAL_ENV_PATH).update_setting(keys, value)
+    check_settings = Settings(ZET_LOCAL_ENV_PATH).get_setting("defaults")
+    assert value ==  check_settings["editor"]["name"]
+
+def test_refresh(zet_settings):
+    """
+    Ensures that we can refresh the object
+    state from the JSON file if it is changed
+    during the program's call stack.
+    """
+    keys = ["defaults", "editor", "name"]
+    value = "vscode"
+    settings = Settings(ZET_LOCAL_ENV_PATH)
+    settings.update_setting(keys, value)
+
+    assert value != settings.get_setting("defaults")["editor"]["name"]
+
+    check_settings = settings.refresh().get_setting("defaults")
+    assert value == check_settings["editor"]["name"]
 
