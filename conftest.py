633b1ea7377bc076ca51a4b35906c7beba4b71ab
mattdood <mawimberly@csumb.edu>
2021-12-27 18:08:09 -0800

    cleanup of zet fixtures, docs, and migrated to Zet() obj

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from typing import List
 
 import pytest
 
-from src.zet.create import create_zet
+from src.zet.create import create_zet, Zet
 from src.zet.git_commands import git_add_zets, git_commit_zets, git_init_zets
 from src.zet.list import list_zets
 from src.zet.settings import (
@@ -19,94 +19,86 @@ from zet.env_setup import add_repo, generate_env
 @pytest.fixture()
 def zet_settings() -> str:
     generate_env()
-    repo_name = "test_repo"
-    add_repo(repo_name)
-    return repo_name
-
-
-@pytest.fixture
-def zet_test_repo(zet_settings) -> str:
-    repo_name = "test_repo"
-    add_repo(repo_name)
-    return repo_name
+    add_repo("zets", "zets/")
+    return "zets" # default repo name returned for subsequent
 
 
 @pytest.fixture
 def zet(
-    zet_test_repo,
+    zet_settings,
 ) -> str:
-    sample_zet = create_zet(
-        "some title",
-        "some category",
-        "some, tags",
-        zet_test_repo
+    sample_zet = Zet().create(
+        title="some title",
+        category="some category",
+        tags="some, tags",
+        zet_repo=zet_settings,
     )
-    return sample_zet
+    return sample_zet.path
 
 
 @pytest.fixture
 def zet_list(
-    zet_test_repo
+    zet_settings,
 ) -> List[str]:
 
     for i in range(5):
         time.sleep(1)
-        create_zet(
+        Zet().create(
             "some title",
             "some category",
             "some, tags",
-            zet_test_repo
+            zet_settings
         )
-    sample_zets = list_zets(zet_test_repo)
+    sample_zets = list_zets(zet_settings)
 
     return sample_zets
 
 
 @pytest.fixture
 def zet_list_paths(
-    zet_test_repo
+    zet_settings,
 ) -> List[str]:
 
     for i in range(5):
         time.sleep(1)
-        create_zet(
+        Zet().create(
             "some title",
             "some category",
             "some, tags",
-            zet_test_repo
+            zet_settings
         )
-    sample_zets = list_zets(zet_test_repo, True)
+    sample_zets = list_zets(zet_settings, True)
 
     return sample_zets
 
 
 @pytest.fixture
-def zet_git_repo(zet_test_repo):
+def zet_git_repo(zet_settings):
     try:
-        git_init_zets(zet_test_repo)
+        git_init_zets(zet_settings)
     except subprocess.CalledProcessError as error:
         error_dict = {"response_code": error.returncode, "output": error.output}
         return error_dict
 
 
 @pytest.fixture
-def zet_git_repo_changes(zet_test_repo, zet_git_repo) -> str:
-    repo_path = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")[zet_test_repo]
+def zet_git_repo_changes(zet_settings, zet_git_repo) -> str:
+    repo_path = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")[zet_settings]
     new_file_path = os.path.join(repo_path["folder"], "some_file.md")
     new_file = open(new_file_path, "w")
     new_file.writelines(["some text", "some other text"])
     new_file.close()
-    git_add_zets(zet_test_repo)
+    git_add_zets(zet_settings)
 
 
-@pytest.fixture(autouse=True)
-def cleanup_run():
-    """Removes workspaces on teardown."""
-    yield
-    default_workspace = "zets/"
-    other_workspace = "other/"
-    if os.path.exists(default_workspace):
-        shutil.rmtree(default_workspace)
-    if os.path.exists(other_workspace):
-        shutil.rmtree(other_workspace)
+# @pytest.fixture(autouse=True)
+# def cleanup_run():
+#     """Removes workspaces on teardown."""
+#     yield
+#     default_workspace = "zets/"
+#     other_workspace = "other/"
+#     if os.path.exists(default_workspace):
+#         shutil.rmtree(default_workspace)
+#     if os.path.exists(other_workspace):
+#         shutil.rmtree(other_workspace)
 
