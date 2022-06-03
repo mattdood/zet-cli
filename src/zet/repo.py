327db033808cddc72b89af30bc93e1f1d100662b
mattdood <mawimberly@csumb.edu>
2022-06-03 01:01:04 -0700

    Repo: New repo module for repository representations

---
+++ src/zet/repo.py
@@ -0,0 +1,44 @@
+import os
+
+from .settings import Settings, ZET_INSTALL_PATH, ZET_LOCAL_ENV_PATH
+
+settings = Settings(ZET_LOCAL_ENV_PATH)
+
+
+def add_repo(zet_repo: str, zet_path: str = None, template: str = None) -> None:
+    """Adds a new repo (folder) and appends to the env file.
+
+    Params:
+        zet_repo (str): A zet repo name to
+            append to an existing env file.
+            The folder is created.
+        zet_path (str|None): The path to a new
+            zet repo. This is the parent folder, defaults
+            to the installation location.
+        template (str|None): A default template to
+            use for the new repository.
+
+    Returns:
+        None
+    """
+
+    # zet folder naming setup
+    # and creation
+    clean_zet_repo = zet_repo.replace(' ', '_')
+    zet_repo_path = os.path.join(
+        zet_path if zet_path else ZET_INSTALL_PATH,
+        clean_zet_repo
+    )
+    if not os.path.exists(zet_repo_path):
+        os.makedirs(zet_repo_path)
+
+    # settings repo update with new
+    # folder that was just created
+    new_repo = {
+        clean_zet_repo: {
+            "folder": zet_repo_path,
+            "template": template if template else settings.get_default_template()
+        }
+    }
+    settings.append_setting("zet_repos", new_repo)
+
