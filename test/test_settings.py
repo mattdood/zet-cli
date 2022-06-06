aab73114f8e40d03e2ce81fc796ff6e9da1834f2
mattdood <mawimberly@csumb.edu>
2022-06-05 19:33:48 -0700

    Test: Settings tests have no constants, combined test_refresh and test_settings_update_key

--- test/test_settings.py
+++ test/test_settings.py
@@ -1,14 +1,11 @@
 import pytest
 
-from src.zet.settings import ZET_LOCAL_ENV_PATH, Settings
-
 
 def test_settings_retrieval(zet_settings):
-    default_settings = Settings(ZET_LOCAL_ENV_PATH).get_setting("defaults")
+    default_settings = zet_settings.get_setting("defaults")
     assert isinstance(default_settings, dict)
 
-    settings = Settings(ZET_LOCAL_ENV_PATH)
-    data = settings.get_setting()
+    data = zet_settings.get_setting()
     assert data is not None
     assert isinstance(data, dict)
 
@@ -21,32 +18,23 @@ def test_settings_append_dict(zet_settings):
             "template": "default"
         }
     }
-    Settings(ZET_LOCAL_ENV_PATH).append_setting(key, value)
-    new_settings = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")
-    assert any(value["some_name"] == new_settings[repo] for repo in new_settings)
+    zet_settings.append_setting(key, value)
+    new_settings = zet_settings.refresh().get_setting("zet_repos")
+    assert value["some_name"] == new_settings["some_name"]
 
 
-def test_settings_update_key(zet_settings):
-    keys = ["defaults", "editor", "name"]
-    value = "vscode"
-    Settings(ZET_LOCAL_ENV_PATH).update_setting(keys, value)
-    check_settings = Settings(ZET_LOCAL_ENV_PATH).get_setting("defaults")
-    assert value == check_settings["editor"]["name"]
-
-
-def test_refresh(zet_settings):
+def test_settings_update_key_refresh(zet_settings):
     """
     Ensures that we can refresh the object
     state from the JSON file if it is changed
     during the program's call stack.
     """
+
     keys = ["defaults", "editor", "name"]
     value = "vscode"
-    settings = Settings(ZET_LOCAL_ENV_PATH)
-    settings.update_setting(keys, value)
+    zet_settings.update_setting(keys, value)
+    assert value != zet_settings.get_setting("defaults")["editor"]["name"]
 
-    assert value != settings.get_setting("defaults")["editor"]["name"]
-
-    check_settings = settings.refresh().get_setting("defaults")
+    check_settings = zet_settings.refresh().get_setting("defaults")
     assert value == check_settings["editor"]["name"]
 
