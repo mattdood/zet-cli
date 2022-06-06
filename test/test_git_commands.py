9328865949ea95d38365de528f98e0bdd53682ff
mattdood <mawimberly@csumb.edu>
2022-06-05 19:33:14 -0700

    Test: Git commands now are tested with new settings, constants removed

--- test/test_git_commands.py
+++ test/test_git_commands.py
@@ -6,23 +6,21 @@ from src.zet.git_commands import git_add_zets
 from src.zet.git_commands import git_commit_zets
 from src.zet.git_commands import git_init_zets
 from src.zet.git_commands import git_push_zets
-from src.zet.settings import Settings, ZET_LOCAL_ENV_PATH
 
 
 def test_git_init_initializes(zet_settings):
-    settings = Settings(ZET_LOCAL_ENV_PATH).get_setting("zet_repos")
-    assert os.path.exists(settings[zet_settings]["folder"])
-    git_init = git_init_zets(zet_settings)
+    assert os.path.exists(zet_settings.get_default_repo_path())
+    git_init = git_init_zets(zet_settings.get_default_repo())
     assert "Initialized empty Git repository" in str(git_init)
 
 
 def test_git_add_adds_changes(zet_settings, zet_git_repo):
-    git_add = git_add_zets(zet_settings)
+    git_add = git_add_zets(zet_settings.get_default_repo())
     assert "" in str(git_add)
 
 
-def test_git_commit_zets(zet_git_repo_changes):
-    git_commit = git_commit_zets("some message", "zets")
+def test_git_commit_zets(zet_settings, zet_git_repo_changes):
+    git_commit = git_commit_zets("some message", zet_settings.get_default_repo())
     assert "some message" in str(git_commit)
     assert "files changed" in str(git_commit) or "file changed" in str(git_commit)
 
