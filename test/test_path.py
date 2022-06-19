195e756888723fa07d395d4a0544701445164e1b
mattdood <mawimberly@csumb.edu>
2022-06-19 15:37:19 -0700

    Fix lint

--- test/test_path.py
+++ test/test_path.py
@@ -2,6 +2,7 @@ import pytest
 
 from zet.settings import ZET_HOME, ZET_INSTALL_PATH
 
+
 def test_path(zet_settings):
     """Tests install path is correct"""
     assert ZET_INSTALL_PATH == ZET_HOME / "zet/"
