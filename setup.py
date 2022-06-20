a2b2907ae21e35dcacb2b4f02a055ed1f6ff114c
mattdood <mawimberly@csumb.edu>
2022-06-19 21:46:33 -0700

    CI: Update readme + setup + add license

--- setup.py
+++ setup.py
@@ -1,5 +1,29 @@
-#!/usr/bin/env python
+import setuptools
 
-from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup()
+setuptools.setup(
+    name="zet-cli",
+    version="0.0.1",
+    author="Matthew Wimberly",
+    author_email="matthew.wimb@gmail.com",
+    description="A Zettlekasten CLI implementation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mattdood/zet-cli",
+    project_urls={
+        "Bug Tracker": "https://github.com/mattdood/zet-cli/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    entry_points={
+        "console_scripts": ["zet=zet:main"]
+    },
+    python_requires=">=3.6",
+)
